1d2621d4f96fb203ff7903ecf9110dd2b6583ebb
A.Issakh <61436438+kateAustin01@users.noreply.github.com>
2025-04-13 14:34:11 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,17 @@
 // @ts-check
-import { defineConfig, devices } from "@playwright/test";
+const { defineConfig, devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-export default defineConfig({
-  testDir: "./tests",
+module.exports = defineConfig({
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,37 +21,45 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
     headless: false,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-        viewport: { width: 1850, height: 1000 },
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],
+        viewport: {width: 1850, height: 1000} 
       },
     },
-    /*
+
+/*    
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { 
+        ...devices['Desktop Firefox'],
+        viewport: {width: 1850, height: 1000} 
+       },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { 
+        ...devices['Desktop Safari'],
+        viewport: {width: 1850, height: 1000}  
+      },
     },
 */
+   
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
